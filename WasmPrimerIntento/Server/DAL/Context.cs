f998994e4af727d94925988c90490616018bd9a4
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-09 20:30:02 -0400

    Agregando el modelo en el Context

--- WasmPrimerIntento/Server/DAL/Context.cs
+++ WasmPrimerIntento/Server/DAL/Context.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using WasmPrimerIntento.Server.Pages;
 
 public class Context : DbContext
 {
+    public DbSet<Aportes> Aportes {get; set;}   
 
     public Context(DbContextOptions<Context> options) : base(options) { }
 }
